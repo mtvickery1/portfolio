396b61cd14902a0db4cab1ea9e4e81f429f28e4d
mv04 <mtvickery1@gmail.com>
2018-10-23 17:43:25 -0500

    filter feature added to projects section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -119,3 +119,28 @@ $(function () {
         }
     );
 });
+
+
+
+
+
+// Initialize Isotope
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+});
+
+// bind filter button click
+$('.filters-button-group').on('click', 'span', function () {
+    var filterValue = $(this).attr('data-filter');
+
+    $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'span', function () {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+    });
+});
