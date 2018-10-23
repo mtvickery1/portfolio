19d30c8e8894c7d68e884456b8669a879f3035b7
mv04 <mtvickery1@gmail.com>
2018-10-23 14:23:32 -0500

    redoing projects section & code cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -97,3 +97,17 @@ $(".side-contact").click(function () {
     }, 'slow');
 });
 
+/////////////////////////////////////////////////////////////////////////
+// PROJECTS
+/////////////////////////////////////////////////////////////////////////
+
+// HOVER
+$(function () {
+    $('.card').hover(
+        function () {
+            $(this).find('> .card-image > img.activator').click();
+        }, function () {
+            $(this).find('> .card-reveal > .card-title').click();
+        }
+    );
+});
