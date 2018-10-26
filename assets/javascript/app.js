91ec660ee9fe72bc11f640e21bb122b526200ed2
mv04 <mtvickery1@gmail.com>
2018-10-25 19:38:32 -0500

    adjusted nav color change

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,44 +15,54 @@ var $grid = $('.grid').isotope({
     layoutMode: 'fitRows'
 });
 
-//======================================================================
-// CHANGE NAV COLOR
-//======================================================================
+// Document Ready
 $(document).ready(function () {
 
-    // Initial nav color on load
-    if ($(window).scrollTop() > 500) {
+    //======================================================================
+    // RESPONSIVE ABOUT BACKGROUND
+    //======================================================================
+    // vpw = $(window).width();
+    // viewport height
+    var vph;
+    var nvh = $('#navbar').height();
+    var scrollPosition = $(window).scrollTop();
+    var landHeight = $('.gorge').height();
+    var triggerPosition = landHeight - nvh;
+
+    console.log('triggerPosition', triggerPosition);
+
+    //======================================================================
+    // SET NAV ON LOAD
+    //======================================================================
+    if (scrollPosition >= triggerPosition) {
+        console.log('landHeight', landHeight);
+        console.log('scrollPosition', scrollPosition);
+
+        // Set nav color on load       
         $("nav").addClass("scrolled");
         $("nav div ul li a" && "nav div a").addClass("scrolled-font");
     }
 
+    //======================================================================
+    // SET NAV ON SCROLL
+    //======================================================================
     $(window).scroll(function () {
+        nvh = $('#navbar').height();
+        scrollPosition = $(window).scrollTop();
+        landHeight = $('.gorge').height();
+        triggerPosition = landHeight - nvh;
+        console.log('landHeight', landHeight);
+        console.log('scrollPosition', scrollPosition);
+        console.log('nvh', nvh);
+        console.log('triggerPosition', triggerPosition);
         // Nav background color change
-        if ($(window).scrollTop() > 500) {
+        if (scrollPosition >= triggerPosition) {
             $("nav").addClass("scrolled");
             $("nav div ul li a" && "nav div a").addClass("scrolled-font");
         } else {
             $("nav").removeClass("scrolled");
             $("nav div ul li a" && "nav div a").removeClass("scrolled-font");
         }
-        // // Nav scroll highlight feature - About
-        // if ($(window).scrollTop() > 885) {
-        //     $(".nav-about").parent().addClass("highlight");
-        //     if ($(window).scrollTop() > 1429) {
-        //         $(".nav-effect").removeClass("highlight");
-        //     }
-        // } else {
-        //     $(".nav-effect").removeClass("highlight");
-        // }
-        // // Nav scroll highlight feature - Projects
-        // if ($(window).scrollTop() > 1429) {
-        //     $(".nav-portfolio").parent().addClass("highlight");
-        //     if ($(window).scrollTop() > 1500) {
-        //         $(".nav-effect").removeClass("highlight");
-        //     }
-        // } else {
-        //     $(".nav-effect").removeClass("highlight");
-        // }
     });
 
     // Nav click highlight feature
@@ -61,87 +71,73 @@ $(document).ready(function () {
         return (false);   // no default behavior from clicking on the link
     });
 
-});
-
-//======================================================================
-// RESPONSIVE ABOUT BACKGROUND
-//======================================================================
-// Nav height
-// vpw = $(window).width();
-var vph;
-var nvh;
-
-// Set height on load
-$(document).ready(function () {
+    // Set height on load
     vph = $(window).height();
     nvh = $('#navbar').height();
     $('.trees').css({ 'height': vph - nvh + 'px' });
     console.log('vph', vph);
     console.log('nvh', nvh);
-});
-
-// Set height on resize
-$(window).resize(function() {
-    vph = $(window).height();
-    nvh = $('#navbar').height();
-    $('.trees').css({ 'height': vph - nvh + 'px' });
-    console.log('vph', vph);
-    console.log('nvh', nvh);
-});
 
 
-//======================================================================
-// SMOOTH SCROLL ON CLICK
-//======================================================================
-var fromTop = 0;
-// Main
-$(".main-about").click(function () {
-    fromTop = 0;
-    $('html,body').animate({
-        scrollTop: $(".trees").offset().top - fromTop - nvh
-    }, 'slow');
-});
-$(".main-projects").click(function () {
-    fromTop = 10;
-    $('html,body').animate({
-        scrollTop: $("#projects").offset().top - fromTop - nvh
-    }, 'slow');
-});
-$(".main-contact").click(function () {
-    $('html,body').animate({
-        scrollTop: $("footer").offset().top - fromTop - nvh
-    }, 'slow');
-});
+    // Set height on resize
+    $(window).resize(function () {
+        vph = $(window).height();
+        nvh = $('#navbar').height();
+        $('.trees').css({ 'height': vph - nvh + 'px' });
+        console.log('vph', vph);
+        console.log('nvh', nvh);
+    });
 
-// Mobile
-$(".side-about").click(function () {
-    fromTop = 0;
-    $('html,body').animate({
-        scrollTop: $(".trees").offset().top - fromTop - nvh
-    }, 'slow');
-});
-$(".side-projects").click(function () {
-    fromTop = 10;
-    $('html,body').animate({
-        scrollTop: $("#projects").offset().top - fromTop - nvh
-    }, 'slow');
-});
-$(".side-contact").click(function () {
-    $('html,body').animate({
-        scrollTop: $("footer").offset().top - fromTop - nvh
-    }, 'slow');
-});
 
-//======================================================================
-// HEADER
-//======================================================================
-// Fadeaway hello-container
-$(window).scroll(function () {
-    $("#hello-container").css("opacity", 1 - $(window).scrollTop() / 250);
-});
+    //======================================================================
+    // SMOOTH SCROLL ON CLICK
+    //======================================================================
+    var fromTop = 0;
+    // Main
+    $(".main-about").click(function () {
+        fromTop = 0;
+        $('html,body').animate({
+            scrollTop: $(".trees").offset().top - fromTop - nvh
+        }, 'slow');
+    });
+    $(".main-projects").click(function () {
+        fromTop = 10;
+        $('html,body').animate({
+            scrollTop: $("#projects").offset().top - fromTop - nvh
+        }, 'slow');
+    });
+    $(".main-contact").click(function () {
+        $('html,body').animate({
+            scrollTop: $("footer").offset().top - fromTop - nvh
+        }, 'slow');
+    });
 
-// Hiding hello-container once scrolled
-$(document).ready(function () {
+    // Mobile
+    $(".side-about").click(function () {
+        fromTop = 0;
+        $('html,body').animate({
+            scrollTop: $(".trees").offset().top - fromTop - nvh
+        }, 'slow');
+    });
+    $(".side-projects").click(function () {
+        fromTop = 10;
+        $('html,body').animate({
+            scrollTop: $("#projects").offset().top - fromTop - nvh
+        }, 'slow');
+    });
+    $(".side-contact").click(function () {
+        $('html,body').animate({
+            scrollTop: $("footer").offset().top - fromTop - nvh
+        }, 'slow');
+    });
+
+    //======================================================================
+    // HEADER
+    //======================================================================
+    // Fadeaway hello-container
+    $(window).scroll(function () {
+        $("#hello-container").css("opacity", 1 - $(window).scrollTop() / 250);
+    });
 
     // Hiding on load
     if ($(window).scrollTop() > 285) {
@@ -150,47 +146,45 @@ $(document).ready(function () {
         $("#hello-container").css("display", "flex");
     }
 
+    // Hiding div on scroll
     $(window).scroll(function () {
-
-        // Hiding div on scroll
         if ($(window).scrollTop() > 285) {
             $("#hello-container").css("display", "none");
-
         } else {
             $("#hello-container").css("display", "flex");
         }
     });
-});
 
-//======================================================================
-// PROJECTS
-//======================================================================
-
-// HOVER
-$(function () {
-    $('.card').hover(
-        function () {
-            $(this).find('> .card-image > img.activator').click();
-        }, function () {
-            $(this).find('> .card-reveal > .card-title').click();
-        }
-    );
-});
-
-//======================================================================
-// ISOTOPE - Layout Plugin
-//======================================================================
-// bind filter button click
-$('.filters-button-group').on('click', 'span', function () {
-    var filterValue = $(this).attr('data-filter');
-
-    $grid.isotope({ filter: filterValue });
-});
-// change is-checked class on buttons
-$('.button-group').each(function (i, buttonGroup) {
-    var $buttonGroup = $(buttonGroup);
-    $buttonGroup.on('click', 'span', function () {
-        $buttonGroup.find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
+    //======================================================================
+    // PROJECTS
+    //======================================================================
+    // Hover over project thumbnail
+    $(function () {
+        $('.card').hover(
+            function () {
+                $(this).find('> .card-image > img.activator').click();
+            }, function () {
+                $(this).find('> .card-reveal > .card-title').click();
+            }
+        );
     });
+
+    //======================================================================
+    // ISOTOPE - Layout Plugin
+    //======================================================================
+    // bind filter button click
+    $('.filters-button-group').on('click', 'span', function () {
+        var filterValue = $(this).attr('data-filter');
+
+        $grid.isotope({ filter: filterValue });
+    });
+    // change is-checked class on buttons
+    $('.button-group').each(function (i, buttonGroup) {
+        var $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'span', function () {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
+    });
+
 });
