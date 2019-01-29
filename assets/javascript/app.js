e30c74691d486fb368606970600f828bcbcaf14e
mv04 <mtvickery1@gmail.com>
2019-01-29 17:49:41 -0600

    fixed preloader

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,115 +2,56 @@
 //======================================================================
 // PRELOADER
 //======================================================================
-$(window).on('load', function() {
-    // Animate loader off screen
-    $(".se-pre-con").fadeOut("slow");;
-});
-
-//======================================================================
-// Initialize
-//======================================================================
-// Initialize Sidenav
-$(document).ready(function () {
-    $('.sidenav').sidenav();
-});
-
-// Initialize AOS
-AOS.init();
-
-// Initialize Isotope
-var $grid = $('.grid').isotope({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
-});
-
 // Document Ready
 $(document).ready(function () {
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");;
 
-    //======================================================================
-    // RESPONSIVE ABOUT BACKGROUND
-    //======================================================================
-    // vpw = $(window).width();
-    // viewport height
-    var vph;
-    var nvh = $('#navbar').height();
-    var scrollPosition = $(window).scrollTop();
-    var landHeight = $('.gorge').height();
-    var aboutTrigger = landHeight - nvh;
 
-    console.log('aboutTrigger', aboutTrigger);
+  //======================================================================
+  // Initialize
+  //======================================================================
+  // Initialize Sidenav
+  $('.sidenav').sidenav();
 
-    //======================================================================
-    // SET NAV COLOR ON LOAD
-    //======================================================================
-    if (scrollPosition >= aboutTrigger) {
-        console.log('landHeight', landHeight);
-        console.log('scrollPosition', scrollPosition);
+  // Initialize AOS
+  AOS.init();
 
-        // Set nav color on load       
-        $("nav").addClass("scrolled");
-        $("nav div ul li a" && "nav div a").addClass("scrolled-font");
-    }
+  // Initialize Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows'
+  });
 
-    //======================================================================
-    // SET NAV COLOR ON SCROLL
-    //======================================================================
-    $(window).scroll(function () {
-        nvh = $('#navbar').height();
-        scrollPosition = $(window).scrollTop();
-        landHeight = $('.gorge').height();
-        var aboutHeight = $('.trees').height();
-        // var projectsHeight = $('.project-section').height();
-        aboutTrigger = landHeight - nvh;
-        var projectsTrigger = aboutTrigger + aboutHeight + 10;
-        var contactTrigger = $(document).height() - $(window).height() - 10;
+  //======================================================================
+  // RESPONSIVE ABOUT BACKGROUND
+  //======================================================================
+  // vpw = $(window).width();
+  // viewport height
+  var vph;
+  var nvh = $('#navbar').height();
+  var scrollPosition = $(window).scrollTop();
+  var landHeight = $('.gorge').height();
+  var aboutTrigger = landHeight - nvh;
 
-        console.log('landHeight', landHeight);
-        console.log('scrollPosition', scrollPosition);
-        console.log('nvh', nvh);
-        console.log('aboutTrigger', aboutTrigger);
-        console.log('projectsTrigger', projectsTrigger);
-        console.log('contactTrigger', contactTrigger);
+  console.log('aboutTrigger', aboutTrigger);
 
-        // Nav background color change
-        if (scrollPosition >= aboutTrigger) {
-            $("nav").addClass("scrolled");
-            $("nav div ul li a" && "nav div a").addClass("scrolled-font");
-        } else {
-            $("nav").removeClass("scrolled");
-            $("nav div ul li a" && "nav div a").removeClass("scrolled-font");
-        }
+  //======================================================================
+  // SET NAV COLOR ON LOAD
+  //======================================================================
+  if (scrollPosition >= aboutTrigger) {
+    console.log('landHeight', landHeight);
+    console.log('scrollPosition', scrollPosition);
 
-        //======================================================================
-        // CUSTOM SCROLLSPY ON SCROLL
-        //======================================================================
-        // ABOUT
-        if (scrollPosition >= aboutTrigger && scrollPosition < projectsTrigger) {
-            $(".nav-about").addClass('highlight').siblings().removeClass('highlight');
-        } else {
-            $(".nav-about").removeClass('highlight');
-        }
+    // Set nav color on load       
+    $("nav").addClass("scrolled");
+    $("nav div ul li a" && "nav div a").addClass("scrolled-font");
+  }
 
-        // PROJECTS
-        if (scrollPosition >= projectsTrigger && scrollPosition < contactTrigger) {
-            $(".nav-projects").addClass('highlight').siblings().removeClass('highlight');
-        } else {
-            $(".nav-projects").removeClass('highlight');
-        }
-
-        // CONTACT
-        if (scrollPosition >= contactTrigger) {
-            $(".nav-contact").addClass('highlight').siblings().removeClass('highlight');
-            $(".nav-about").removeClass('highlight');
-            $(".nav-projects").removeClass('highlight');
-        } else {
-            $(".nav-contact").removeClass('highlight');
-        }
-    });
-
-    //======================================================================
-    // CUSTOM SCROLLSPY ON LOAD
-    //======================================================================
+  //======================================================================
+  // SET NAV COLOR ON SCROLL
+  //======================================================================
+  $(window).scroll(function () {
     nvh = $('#navbar').height();
     scrollPosition = $(window).scrollTop();
     landHeight = $('.gorge').height();
@@ -119,154 +60,209 @@ $(document).ready(function () {
     aboutTrigger = landHeight - nvh;
     var projectsTrigger = aboutTrigger + aboutHeight + 10;
     var contactTrigger = $(document).height() - $(window).height() - 10;
+
+    console.log('landHeight', landHeight);
+    console.log('scrollPosition', scrollPosition);
+    console.log('nvh', nvh);
+    console.log('aboutTrigger', aboutTrigger);
+    console.log('projectsTrigger', projectsTrigger);
+    console.log('contactTrigger', contactTrigger);
+
+    // Nav background color change
+    if (scrollPosition >= aboutTrigger) {
+      $("nav").addClass("scrolled");
+      $("nav div ul li a" && "nav div a").addClass("scrolled-font");
+    } else {
+      $("nav").removeClass("scrolled");
+      $("nav div ul li a" && "nav div a").removeClass("scrolled-font");
+    }
+
+    //======================================================================
+    // CUSTOM SCROLLSPY ON SCROLL
+    //======================================================================
     // ABOUT
     if (scrollPosition >= aboutTrigger && scrollPosition < projectsTrigger) {
-        $(".nav-about").addClass('highlight').siblings().removeClass('highlight');
+      $(".nav-about").addClass('highlight').siblings().removeClass('highlight');
     } else {
-        $(".nav-about").removeClass('highlight');
+      $(".nav-about").removeClass('highlight');
     }
 
     // PROJECTS
     if (scrollPosition >= projectsTrigger && scrollPosition < contactTrigger) {
-        $(".nav-projects").addClass('highlight').siblings().removeClass('highlight');
+      $(".nav-projects").addClass('highlight').siblings().removeClass('highlight');
     } else {
-        $(".nav-projects").removeClass('highlight');
+      $(".nav-projects").removeClass('highlight');
     }
 
     // CONTACT
     if (scrollPosition >= contactTrigger) {
-        $(".nav-contact").addClass('highlight').siblings().removeClass('highlight');
-        $(".nav-about").removeClass('highlight');
-        $(".nav-projects").removeClass('highlight');
+      $(".nav-contact").addClass('highlight').siblings().removeClass('highlight');
+      $(".nav-about").removeClass('highlight');
+      $(".nav-projects").removeClass('highlight');
     } else {
-        $(".nav-contact").removeClass('highlight');
+      $(".nav-contact").removeClass('highlight');
     }
+  });
+
+  //======================================================================
+  // CUSTOM SCROLLSPY ON LOAD
+  //======================================================================
+  nvh = $('#navbar').height();
+  scrollPosition = $(window).scrollTop();
+  landHeight = $('.gorge').height();
+  var aboutHeight = $('.trees').height();
+  // var projectsHeight = $('.project-section').height();
+  aboutTrigger = landHeight - nvh;
+  var projectsTrigger = aboutTrigger + aboutHeight + 10;
+  var contactTrigger = $(document).height() - $(window).height() - 10;
+  // ABOUT
+  if (scrollPosition >= aboutTrigger && scrollPosition < projectsTrigger) {
+    $(".nav-about").addClass('highlight').siblings().removeClass('highlight');
+  } else {
+    $(".nav-about").removeClass('highlight');
+  }
+
+  // PROJECTS
+  if (scrollPosition >= projectsTrigger && scrollPosition < contactTrigger) {
+    $(".nav-projects").addClass('highlight').siblings().removeClass('highlight');
+  } else {
+    $(".nav-projects").removeClass('highlight');
+  }
+
+  // CONTACT
+  if (scrollPosition >= contactTrigger) {
+    $(".nav-contact").addClass('highlight').siblings().removeClass('highlight');
+    $(".nav-about").removeClass('highlight');
+    $(".nav-projects").removeClass('highlight');
+  } else {
+    $(".nav-contact").removeClass('highlight');
+  }
 
 
-    //======================================================================
-    // RESPONSIVE ABOUT SECTION
-    //======================================================================
-    // Set height on load
+  //======================================================================
+  // RESPONSIVE ABOUT SECTION
+  //======================================================================
+  // Set height on load
+  vph = $(window).height();
+  nvh = $('#navbar').height();
+  $('.trees').css({ 'height': vph - nvh + 'px' });
+  console.log('vph', vph);
+  console.log('nvh', nvh);
+
+
+  // Set height on resize - This may be causing with mobile scroll hiccups
+  $(window).resize(function () {
     vph = $(window).height();
     nvh = $('#navbar').height();
     $('.trees').css({ 'height': vph - nvh + 'px' });
     console.log('vph', vph);
     console.log('nvh', nvh);
+  });
 
+  //======================================================================
+  // HIGHLIGHT SECTION ON CLICK
+  //======================================================================
+  $('.nav-effect a').click(function () {
+    $(this).closest("a").addClass('highlight').siblings().removeClass('highlight');
+    return (false);   // no default behavior from clicking on the link
+  });
 
-    // Set height on resize - This may be causing with mobile scroll hiccups
-    $(window).resize(function () {
-        vph = $(window).height();
-        nvh = $('#navbar').height();
-        $('.trees').css({ 'height': vph - nvh + 'px' });
-        console.log('vph', vph);
-        console.log('nvh', nvh);
-    });
+  //======================================================================
+  // SMOOTH SCROLL ON CLICK
+  //======================================================================
+  var fromTop = 0;
+  // Main
+  $(".main-about").click(function () {
+    fromTop = 0;
+    $('html,body').animate({
+      scrollTop: $(".trees").offset().top - fromTop - nvh
+    }, 'slow');
+  });
+  $(".main-projects").click(function () {
+    fromTop = 10;
+    $('html,body').animate({
+      scrollTop: $("#projects").offset().top - fromTop - nvh
+    }, 'slow');
+  });
+  $(".main-contact").click(function () {
+    $('html,body').animate({
+      scrollTop: $("footer").offset().top - fromTop - nvh
+    }, 'slow');
+  });
 
-    //======================================================================
-    // HIGHLIGHT SECTION ON CLICK
-    //======================================================================
-    $('.nav-effect a').click(function () {
-        $(this).closest("a").addClass('highlight').siblings().removeClass('highlight');
-        return (false);   // no default behavior from clicking on the link
-    });
+  // Mobile
+  $(".side-about").click(function () {
+    fromTop = 0;
+    $('html,body').animate({
+      scrollTop: $(".trees").offset().top - fromTop - nvh
+    }, 'slow');
+  });
+  $(".side-projects").click(function () {
+    fromTop = 10;
+    $('html,body').animate({
+      scrollTop: $("#projects").offset().top - fromTop - nvh
+    }, 'slow');
+  });
+  $(".side-contact").click(function () {
+    $('html,body').animate({
+      scrollTop: $("footer").offset().top - fromTop - nvh
+    }, 'slow');
+  });
 
-    //======================================================================
-    // SMOOTH SCROLL ON CLICK
-    //======================================================================
-    var fromTop = 0;
-    // Main
-    $(".main-about").click(function () {
-        fromTop = 0;
-        $('html,body').animate({
-            scrollTop: $(".trees").offset().top - fromTop - nvh
-        }, 'slow');
-    });
-    $(".main-projects").click(function () {
-        fromTop = 10;
-        $('html,body').animate({
-            scrollTop: $("#projects").offset().top - fromTop - nvh
-        }, 'slow');
-    });
-    $(".main-contact").click(function () {
-        $('html,body').animate({
-            scrollTop: $("footer").offset().top - fromTop - nvh
-        }, 'slow');
-    });
+  //======================================================================
+  // HEADER
+  //======================================================================
+  // Fadeaway hello-container
+  $(window).scroll(function () {
+    $("#hello-container").css("opacity", 1 - $(window).scrollTop() / 250);
+  });
 
-    // Mobile
-    $(".side-about").click(function () {
-        fromTop = 0;
-        $('html,body').animate({
-            scrollTop: $(".trees").offset().top - fromTop - nvh
-        }, 'slow');
-    });
-    $(".side-projects").click(function () {
-        fromTop = 10;
-        $('html,body').animate({
-            scrollTop: $("#projects").offset().top - fromTop - nvh
-        }, 'slow');
-    });
-    $(".side-contact").click(function () {
-        $('html,body').animate({
-            scrollTop: $("footer").offset().top - fromTop - nvh
-        }, 'slow');
-    });
+  // Hiding on load
+  if ($(window).scrollTop() > 285) {
+    $("#hello-container").css("display", "none");
+  } else {
+    $("#hello-container").css("display", "flex");
+  }
 
-    //======================================================================
-    // HEADER
-    //======================================================================
-    // Fadeaway hello-container
-    $(window).scroll(function () {
-        $("#hello-container").css("opacity", 1 - $(window).scrollTop() / 250);
-    });
-
-    // Hiding on load
+  // Hiding div on scroll
+  $(window).scroll(function () {
     if ($(window).scrollTop() > 285) {
-        $("#hello-container").css("display", "none");
+      $("#hello-container").css("display", "none");
     } else {
-        $("#hello-container").css("display", "flex");
+      $("#hello-container").css("display", "flex");
     }
+  });
 
-    // Hiding div on scroll
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 285) {
-            $("#hello-container").css("display", "none");
-        } else {
-            $("#hello-container").css("display", "flex");
-        }
-    });
+  //======================================================================
+  // PROJECTS
+  //======================================================================
+  // Hover over project thumbnail
+  $(function () {
+    $('.card').hover(
+      function () {
+        $(this).find('> .card-image > img.activator').click();
+      }, function () {
+        $(this).find('> .card-reveal > .card-title').click();
+      }
+    );
+  });
 
-    //======================================================================
-    // PROJECTS
-    //======================================================================
-    // Hover over project thumbnail
-    $(function () {
-        $('.card').hover(
-            function () {
-                $(this).find('> .card-image > img.activator').click();
-            }, function () {
-                $(this).find('> .card-reveal > .card-title').click();
-            }
-        );
-    });
+  //======================================================================
+  // ISOTOPE - Layout Plugin
+  //======================================================================
+  // bind filter button click
+  $('.filters-button-group').on('click', 'span', function () {
+    var filterValue = $(this).attr('data-filter');
 
-    //======================================================================
-    // ISOTOPE - Layout Plugin
-    //======================================================================
-    // bind filter button click
-    $('.filters-button-group').on('click', 'span', function () {
-        var filterValue = $(this).attr('data-filter');
-
-        $grid.isotope({ filter: filterValue });
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', 'span', function () {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $(this).addClass('is-checked');
     });
-    // change is-checked class on buttons
-    $('.button-group').each(function (i, buttonGroup) {
-        var $buttonGroup = $(buttonGroup);
-        $buttonGroup.on('click', 'span', function () {
-            $buttonGroup.find('.is-checked').removeClass('is-checked');
-            $(this).addClass('is-checked');
-        });
-    });
+  });
 
 });
